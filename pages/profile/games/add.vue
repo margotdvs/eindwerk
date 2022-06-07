384dfee5106c8410c96c4b772a81589df82fcbbf
Korneel Eeckhout <korneel@pienter.tech>
2022-06-07 11:57:47 +0200

    post game + image

--- pages/profile/games/add.vue
+++ pages/profile/games/add.vue
@@ -2,19 +2,28 @@
   <div>
     <h1>Add Game</h1>
 
-    <FormKit v-model="data" type="form" submit-label="Save">
+    <FormKit v-model="data" type="form" submit-label="Save" @submit="save">
+      <FormKit
+        type="select"
+        label="Status"
+        name="status"
+        :options="{
+          draft: 'Draft',
+          published: 'Published',
+          archived: 'Archived',
+        }"
+      />
       <FormKit
         type="text"
-        name="title"
+        name="name"
         label="Game title"
         validation="required"
       />
       <FormKit
         type="file"
-        name="title_img"
+        name="title_image"
         label="Title image"
         accept=".jpg, .jpeg .png"
-        validation="required"
       />
       <FormKit
         type="number"
@@ -32,7 +41,7 @@
       />
       <FormKit
         type="textarea"
-        name="short_description"
+        name="description_short"
         label="Short description"
         maxlength="450"
         validation="required"
@@ -43,7 +52,9 @@
 
 <script>
 import { createInput } from '@formkit/vue';
+import { mapState, mapActions } from 'pinia';
 import TagInput from '~/components/TagInput.vue';
+import { useAuthStore } from '~/stores/auth.js';
 
 definePageMeta({
   middleware: ['auth'],
@@ -55,13 +66,86 @@ export default {
     return {
       tagInput: createInput(TagInput),
       data: {
-        title: 'test',
-        title_img: '',
+        status: 'draft',
+        title: '',
+        title_image: [],
         score: '',
-        tags: [1, 3],
-        short_description: '',
+        tags: [],
+        description_short: '',
       },
     };
+  },
+  computed: {
+    ...mapState(useAuthStore, ['accessToken']),
+  },
+  methods: {
+    ...mapActions(useAuthStore, ['logout']),
+    uploadFile(file) {
+      const body = new FormData();
+      body.append('title', file.name);
+      body.append('file', file.file);
+
+      return fetch('https://margot.fullstacksyntra.be/files', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+        body,
+      })
+        .then((response) => {
+          if (response.status === 401) {
+            throw new Error('401');
+          }
+
+          if (!response.ok) {
+            throw new Error('Could not upload file');
+          }
+
+          return response.json();
+        })
+        .then((body) => {
+          return body.data.id;
+        });
+    },
+    save(data) {
+      this.uploadFile(data.title_image[0])
+        .then((fileId) => {
+          data.title_image = fileId;
+          return fetch('https://margot.fullstacksyntra.be/items/games', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${this.accessToken}`,
+            },
+            body: JSON.stringify(data),
+          });
+        })
+        .then((response) => {
+          if (response.status === 401) {
+            throw new Error('401');
+          }
+
+          if (!response.ok) {
+            throw new Error('Could not upload file');
+          }
+
+
+          if (!response.ok) {
+            throw new Error('Could not upload file');
+          }
+
+          return response.json();
+        })
+        .then((body) => {
+          console.log(body);
+        })
+        .catch((err) => {
+          if (err.message === '401') {
+            this.logout();
+          }
+          console.error(err);
+        });
+    },
   },
 };
 </script>
