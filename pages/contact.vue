5801197b0f6fcd17aafbf48f0cd2816959937f92
Margot De Vos <margotdevos1@hotmail.com>
2022-06-13 12:08:01 +0200

    contact page styling

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,19 +1,21 @@
 <template>
-  <h1>Contact</h1>
-  <div class="contact-container">
-    <div class="contact-left">
-      <h3>About Us</h3>
-      <span
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-        necessitatibus, ea deleniti recusandae cum aut repellendus fugiat dolore
-        doloremque non quis. Laborum possimus a eius mollitia, aspernatur minima
-        officiis cumque.</span
-      >
-    </div>
-    <div class="contact-right">
-      <FormKit type="form" submit-label="Send">
-        <FormKit type="textarea" label="Give us your feedback" />
-      </FormKit>
+  <div>
+    <h1>Contact</h1>
+    <div class="contact-container">
+      <div class="contact-left">
+        <h3>About Us</h3>
+        <span
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          necessitatibus, ea deleniti recusandae cum aut repellendus fugiat
+          dolore doloremque non quis. Laborum possimus a eius mollitia,
+          aspernatur minima officiis cumque.</span
+        >
+      </div>
+      <div class="contact-right">
+        <FormKit type="form" submit-label="Send">
+          <FormKit type="textarea" label="Give us your feedback" />
+        </FormKit>
+      </div>
     </div>
   </div>
 </template>
@@ -28,6 +30,7 @@ export default {
 .contact-container {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
   padding-top: 6rem;
 }
 </style>
